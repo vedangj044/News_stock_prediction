c5c2306ab4e4318eebf867f493edf7cd2601e0b1
vedangj044 <vedangj044@gmail.com>
2020-02-12 19:28:13 +0530

    Google News scraper v_alpha

--- news_scraper.py
+++ news_scraper.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 class scraper():
 
-    def __init__(keyword: str, limit=10: int):
-        self.url = f"https://news.google.com/search?q={}&hl=en-IN&gl=IN&ceid=IN:en"
+    def __init__(self, keyword: str, limit=10):
+        self.url = "https://news.google.com/search?q={0}&hl=en-IN&gl=IN&ceid=IN:en"
         self.query = self.url.format("%".join(keyword.split()))
         self.request_news = requests.get(self.query)
 
@@ -13,27 +14,29 @@ class scraper():
         else:
             print("Query not working !!!")
 
-    def link_extract():
+    def link_extract(self):
         self.soup_news = BeautifulSoup(self.request_news.content, "html.parser")
 
         self.results = []
 
-        for link in self.soup_news.find_all('div', class_='r'):
-            a_tag = g.find_all('a')
+        for link in self.soup_news.find_all('div', class_="NiLAwe"):
+            a_tag = link.find_all('a')
             if a_tag:
-                l = anchors[0]['href']
-                title = g.find('h3').text
+                l = a_tag[0]['href']
+                title = link.find('h3').text
                 item = {
                     "title": title,
-                    "link": link
+                    "link": "https://news.google.com/"+l
                 }
                 self.results.append(item)
 
-        self.pretty_print()
-
-    def pretty_print():
-        json.dumps(self.results)
+    def pretty_print(self):
+        data = json.dumps(self.results)
+        f = open("data.json", "w")
+        f.write(data)
+        f.close()
 
 
 if __name__ == "__main__":
-    sc = scraper()
+    sc = scraper("Tesla Stocks")
+    sc.pretty_print()
