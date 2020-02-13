e5354fa87044de50ccf900bdfa192d23c210f50b
vedangj044 <vedangj044@gmail.com>
2020-02-13 13:18:16 +0530

    Made object of predictor

--- predictor.py
+++ predictor.py
@@ -1,14 +1,25 @@
 from classifier import Classify
 from news_scraper import scraper
+import json
 
-if __name__ == "__main__":
-    sc = scraper("Birla stocks").results
+class predict():
 
-    score = 0
-    for i in sc:
-        value = Classify(i["title"]).classify()
-        if value == "Positive":
-            score+=1
+    def __init__(self, keyword):
+        self.score = 0
+        self.articles = 0
+        self.keyword = keyword
+        self.predictor()
 
-    print("Score is: {}".format(score/len(sc)))
-    print("Total articles {}".format(len(sc)))
+    def predictor(self):
+        sc = scraper(self.keyword).results
+        self.articles = len(sc)
+
+        for i in sc:
+            value = Classify(i["title"]).classify()
+            if value == "Positive":
+                self.score+=1
+
+        self.final_pred = self.score/self.articles
+
+    def resp(self):
+        return json.dumps({"score": self.final_pred, "articles": self.articles})
