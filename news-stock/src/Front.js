e0615c7afb32faf299e4410078dba2c5b8ab52e7
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-02-15 01:08:51 +0530

    Frontend Stage1 completed

--- news-stock/src/Front.js
+++ news-stock/src/Front.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -16,6 +16,20 @@ import frontimg from "./img/front.jpg";
 import Container from "@material-ui/core/Container";
 import Chip from "@material-ui/core/Chip";
 import DoneIcon from "@material-ui/icons/Done";
+import frontimg1 from "./img/front1.jpg";
+import Zoom from "react-reveal/Zoom";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import DirectionsIcon from "@material-ui/icons/Directions";
+import Skeleton from "@material-ui/lab/Skeleton";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import TrendingDownIcon from "@material-ui/icons/TrendingDown";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import Cookies from 'universal-cookie';
+import { useHistory } from 'react-router-dom';
 
 function Copyright() {
   return (
@@ -35,7 +49,7 @@ const useStyles = makeStyles(theme => ({
     height: "100vh"
   },
   image: {
-    backgroundImage: `url(${frontimg})`,
+    backgroundImage: `url(${frontimg1})`,
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "dark"
@@ -61,36 +75,100 @@ const useStyles = makeStyles(theme => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
+  },
+  tfield: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 500
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  },
+  divider: {
+    height: 28,
+    margin: 4
   }
 }));
 
 export default function Front() {
+  const cookies = new Cookies();
   const classes = useStyles();
+
+  let history = useHistory();
+
+  const routeChange = () => {
+    let path = `/Dashboard`;
+    cookies.set('userId', comp, { path: '/' });
+    history.push(path);
+  }
+
+  const handleDeleteChip = event => {
+    var index = comp.indexOf(event.target.value);
+    comp.splice(index, 1);
+    setComp(comp);
+  };
+
+  const handleClick = () => {
+    console.info("You clicked the Chip.");
+  };
+
+  const handleEnter = event => {
+    if (event.key === "Enter") {
+      var compadd = event.target.value;
+      if (compadd!=="" && comp.indexOf(compadd)==-1)
+      {
+      setComp([...comp, compadd]);
+      }
+      event.preventDefault();
+    }
+  };
+
+  const handleComp = event => {
+    var compadd = event.target.value;
+    setComp([...comp, compadd]);
+  };
+
+  const loading = false;
+
+  const [comp, setComp] = useState(["Tesla", "Tata"]);
+
+  const [compgrow, setCompgrow] = useState(false);
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Container maxWidth="xl" className={classes.image} />
-      <Container maxWidth="md">
-        <div className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            Sign in
+      <Container maxWidth="xl" className={classes.image}>
+        <Zoom duration="4000">
+          <Typography
+            variant="h1"
+            align="center"
+            style={{
+              fontFamily: "varela round",
+              color: "#3c3428",
+              marginTop: "20%"
+            }}
+          >
+            Famulus
           </Typography>
-          <br />
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <br></br>
 
+          <Typography
+            variant="h3"
+            align="center"
+            style={{ fontFamily: "source sans pro", color: "#3c3428" }}
+          >
+            Your Intelligent Stock Assistant
+          </Typography>
+        </Zoom>
+      </Container>
+      <Container maxWidth="sm" style={{ height: "100%" }}>
+        <div className={classes.paper}>
           <form className={classes.form} noValidate>
-            <TextField
+            {/* <TextField
               
               margin="normal"
               required
@@ -100,25 +178,108 @@ export default function Front() {
               name="company"
               autoComplete="company"
               autoFocus
-            />
-            <Chip
-              
-              label="Tesla"
-              clickable
-              color="primary"
-              
-              deleteIcon={<DoneIcon />}
-            />
-            <Chip
-              
-              label="Tata"
-              clickable
-              color="primary"
-              
-              deleteIcon={<DoneIcon />}
-            />
+            /> */}
+
+            <Paper
+              component="form"
+              className={classes.tfield}
+              style={{ borderRadius: "100px" }}
+            >
+              <IconButton
+                type="submit"
+                className={classes.iconButton}
+                aria-label="search"
+              >
+                <SearchIcon />
+              </IconButton>
+              <InputBase
+                className={classes.input}
+                placeholder="Search Companies of interest"
+                inputProps={{ "aria-label": "search companies" }}
+                // onChange={handleComp}
+                onKeyPress={handleEnter}
+              />
+            </Paper>
+            {comp.map(comp1 => (
+              <Chip
+                variant="outlined"
+                color="primary"
+                avatar={<Avatar>{comp1.charAt(0)}</Avatar>}
+                label={comp1}
+                onClick={handleClick}
+                onDelete={event => handleDeleteChip(event, comp1)}
+                style={{ margin: "20px" }}
+                value={comp1}
+                key={comp1}
+              />
+            ))}
           </form>
         </div>
+        {/* <Grid container>
+          <Box width={500}>
+            <Skeleton variant="rect" width={600} height={400} />
+            <Box pt={0.5}>
+              <Skeleton />
+            </Box>
+          </Box>
+        </Grid> */}
+        <Paper style={{ padding: "30px", width: "600px" }}>
+          <Grid container spacing="8">
+            <Grid item>
+              <Typography variant="h3" style={{ color: "grey" }}>
+                Stock Analysis
+              </Typography>
+            </Grid>
+            <Grid item>
+              {compgrow ? (
+                <TrendingUpIcon
+                  style={{ fontSize: "60px", color: "#2dc937" }}
+                />
+              ) : (
+                <TrendingDownIcon
+                  style={{ fontSize: "60px", color: "#cc3232" }}
+                />
+              )}
+            </Grid>
+          </Grid>
+
+          <Typography variant="h6" color="textSecondary">
+            The stock is expected to {" "}
+            {(compgrow)?
+            (<span
+              style={{
+                fontSize: "35px",
+                borderBottom: "7px solid #2dc937",
+                fontFamily: "varela round",
+                padding: "1px",
+                color: "black",
+                borderRadius: "3px"
+              }}
+            >
+              Rise
+            </span>)
+            :(<span style={{fontSize: "35px",borderBottom:"7px solid #cc3232", fontFamily:"varela round", padding:"1px", color:"black",
+            borderRadius:"3px"}}>Fall</span>)
+}
+          </Typography>
+          {/* <Typography variant="h6" align="left" color="textSecondary">
+            The stock is expected to <span style={{borderBottom:"7px solid #cc3232", fontFamily:"varela round", padding:"1px", color:"black",
+          borderRadius:"3px"}}>Fall</span>
+            </Typography> */}
+        </Paper>
+        <center>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<EqualizerIcon />}
+            style={{ margin: "40px" }}
+            onClick={routeChange}
+          >
+            Deep Analysis
+          </Button>
+        </center>
       </Container>
       {/* <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
