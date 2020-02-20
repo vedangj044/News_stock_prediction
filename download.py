5a80b30791a4cd60ba189066a543a85f9f055a1d
Ritik Jain <ritikjain1272@gmail.com>
2020-02-21 00:13:15 +0530

    Added download.py file

---
+++ download.py
@@ -0,0 +1,15 @@
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('twitter_samples')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
